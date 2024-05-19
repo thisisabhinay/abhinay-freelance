9670ecadd06a09177ad79e8ed426cb81718a41b1
thisisabhinay <50246894+thisisabhinay@users.noreply.github.com>
2024-05-19 11:04:51 +0530

    fix: config schemas for blog and project

---
+++ src/content/config.ts
@@ -0,0 +1,44 @@
+import { z, defineCollection } from "astro:content";
+
+const BlogPosts = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    excerpt: z.string(),
+    category: z.string().trim(),
+    author: z.string().trim(),
+    draft: z.boolean().optional(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+    publishDate: z.string().transform((str) =>
+      new Date(str).toLocaleDateString("en-GB", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })
+    ),
+  }),
+});
+
+const Projects = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    excerpt: z.string(),
+    category: z.string().trim(),
+    author: z.string().trim(),
+    draft: z.boolean().optional(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+    publishDate: z.string().transform((str) =>
+      new Date(str).toLocaleDateString("en-GB", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })
+    ),
+  }),
+});
+
+export const collections = {
+  blog: BlogPosts,
+  project: Projects,
+};
