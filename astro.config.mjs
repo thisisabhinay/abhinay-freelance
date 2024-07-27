aa1920484ec76fa836a39d4c856546f69181f6ee
thisisabhinay <50246894+thisisabhinay@users.noreply.github.com>
2024-07-27 13:02:15 +0530

    feat: add mdx support and rename all content files extension

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), mdx()]
 });
